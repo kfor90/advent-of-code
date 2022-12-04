c7addc114159bfd446b82044faf551a92897bf3e
Keir Forster <keirforster@gmail.com>
2022-12-03 22:28:26 -0800

    build: add license and fix day 1 output order

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as day1 from "./day-1/index.js";
+import * as day1 from './day-1/index.js';
 
-console.log(`Day 1 - Part 1/2: ${await day1.part2()}`);
-console.log(`Day 1 - Part 2/2: ${await day1.part1()}`);
+console.log(`Day 1 - Part 1/2: ${await day1.part1()}`);
+console.log(`Day 1 - Part 2/2: ${await day1.part2()}`);
