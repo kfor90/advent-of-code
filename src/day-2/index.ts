c91ca3f23230d1e6aa75503a4acc36f8b429391a
Keir Forster <keirforster@gmail.com>
2022-12-07 12:57:27 -0800

    feat: complete day 2 part 2 challenge

--- src/day-2/index.ts
+++ src/day-2/index.ts
@@ -1,50 +1,112 @@
 import { getFileIterator } from '../shared/index.js';
 
+const INPUT_FILE_PATH = 'day-2/input.txt';
+
+enum Play {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  X = 'X',
+  Y = 'Y',
+  Z = 'Z',
+}
+
+enum PlayName {
+  Rock = 'A',
+  Paper = 'B',
+  Scissors = 'C',
+}
+
+enum StrategyName {
+  LOSE = 'X',
+  DRAW = 'Y',
+  WIN = 'Z',
+}
+
 const ROCK_PTS = 1;
 const PAPER_PTS = 2;
 const SCISSORS_PTS = 3;
 
 const WIN_PTS = 6;
-const TIE_PTS = 3;
-const LOSS_PTS = 0;
+const DRAW_PTS = 3;
+const LOSE_PTS = 0;
 
-type PointsByPlay = { [key: string]: number };
-
-const PtsByPlay: PointsByPlay = {
-  A: ROCK_PTS,
-  B: PAPER_PTS,
-  C: SCISSORS_PTS,
-  X: ROCK_PTS,
-  Y: PAPER_PTS,
-  Z: SCISSORS_PTS,
+const playNamesByPlay: { [play: string]: PlayName } = {
+  [Play.A]: PlayName.Rock,
+  [Play.B]: PlayName.Paper,
+  [Play.C]: PlayName.Scissors,
 };
 
-const elfResultsByPlay: PointsByPlay = {
-  AX: TIE_PTS,
+const PtsByPlay = {
+  [Play.A]: ROCK_PTS,
+  [Play.B]: PAPER_PTS,
+  [Play.C]: SCISSORS_PTS,
+  [Play.X]: ROCK_PTS,
+  [Play.Y]: PAPER_PTS,
+  [Play.Z]: SCISSORS_PTS,
+};
+
+const ptsByStrategyName = {
+  [StrategyName.LOSE]: LOSE_PTS,
+  [StrategyName.DRAW]: DRAW_PTS,
+  [StrategyName.WIN]: WIN_PTS,
+};
+
+const playsByPlayName = {
+  [PlayName.Rock]: Play.A,
+  [PlayName.Paper]: Play.B,
+  [PlayName.Scissors]: Play.C,
+};
+
+const elfResultsByCombinedPlay: { [key: string]: number } = {
+  AX: DRAW_PTS,
   AY: WIN_PTS,
-  AZ: LOSS_PTS,
-  BX: LOSS_PTS,
-  BY: TIE_PTS,
+  AZ: LOSE_PTS,
+  BX: LOSE_PTS,
+  BY: DRAW_PTS,
   BZ: WIN_PTS,
   CX: WIN_PTS,
-  CY: LOSS_PTS,
-  CZ: TIE_PTS,
+  CY: LOSE_PTS,
+  CZ: DRAW_PTS,
 };
 
-const getResultPts = (opponentPlay: string, elfPlay: string) => {
-  return elfResultsByPlay[`${opponentPlay}${elfPlay}`];
+const elfPlaysByStrategy = {
+  [`${StrategyName.LOSE}:${PlayName.Rock}`]: PlayName.Scissors,
+  [`${StrategyName.LOSE}:${PlayName.Paper}`]: PlayName.Rock,
+  [`${StrategyName.LOSE}:${PlayName.Scissors}`]: PlayName.Paper,
+  [`${StrategyName.DRAW}:${PlayName.Rock}`]: PlayName.Rock,
+  [`${StrategyName.DRAW}:${PlayName.Paper}`]: PlayName.Paper,
+  [`${StrategyName.DRAW}:${PlayName.Scissors}`]: PlayName.Scissors,
+  [`${StrategyName.WIN}:${PlayName.Rock}`]: PlayName.Paper,
+  [`${StrategyName.WIN}:${PlayName.Paper}`]: PlayName.Scissors,
+  [`${StrategyName.WIN}:${PlayName.Scissors}`]: PlayName.Rock,
 };
 
-const getPlayPoints = (play: string) => {
+const getPlay = (opponentPlay: Play, elfStrategy: StrategyName) => {
+  const opponentPlayName = playNamesByPlay[opponentPlay];
+  const strategy = `${elfStrategy}:${opponentPlayName}`;
+  const playName = elfPlaysByStrategy[strategy];
+  return playsByPlayName[playName];
+};
+
+const getResultPts = (opponentPlay: Play, elfPlay: Play) => {
+  return elfResultsByCombinedPlay[`${opponentPlay}${elfPlay}`];
+};
+
+const getPlayPoints = (play: Play) => {
   return PtsByPlay[play];
 };
 
+const getPointsByStrategyName = (elfStrategy: StrategyName) => {
+  return ptsByStrategyName[elfStrategy];
+};
+
 const calculateTotalElfScore = async () => {
-  const fileIterator = getFileIterator('day-2/input.txt');
+  const fileIterator = getFileIterator(INPUT_FILE_PATH);
   let totalElfPts = 0;
 
   for await (const line of fileIterator) {
-    const [opponentPlay, elfPlay] = line.split(' ');
+    const [opponentPlay, elfPlay] = line.split(' ') as [Play, Play];
     const elfPlayPoints = getPlayPoints(elfPlay);
     const elfResultPts = getResultPts(opponentPlay, elfPlay);
     totalElfPts += elfPlayPoints + elfResultPts;
@@ -53,6 +115,26 @@ const calculateTotalElfScore = async () => {
   return totalElfPts;
 };
 
+const calculateTotalElfScoreByStrategy = async () => {
+  const fileIterator = getFileIterator(INPUT_FILE_PATH);
+  let totalElfPts = 0;
+
+  for await (const line of fileIterator) {
+    const [opponentPlay, elfStrategy] = line.split(' ') as [Play, StrategyName];
+    const elfPlay = getPlay(opponentPlay, elfStrategy);
+    const elfPlayPts = getPlayPoints(elfPlay);
+    const elfResultPts = getPointsByStrategyName(elfStrategy);
+
+    totalElfPts += elfPlayPts + elfResultPts;
+  }
+
+  return totalElfPts;
+};
+
 export const part1 = () => {
   return calculateTotalElfScore();
+};
+
+export const part2 = () => {
+  return calculateTotalElfScoreByStrategy();
 };
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ console.log(`Day 1 - Part 1/2: ${await day1.part1()}`);
 console.log(`Day 1 - Part 2/2: ${await day1.part2()}`);
 
 console.log(`Day 2 - Part 1/2: ${await day2.part1()}`);
+console.log(`Day 2 - Part 2/2: ${await day2.part2()}`);
